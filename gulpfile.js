a59d5dc11faf65e6207abbeb75785a97c3f8b4dd
Alexandre Durand <contact.durand.alexandre@gmail.com>
2024-08-30 15:41:09 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+// Déclaration de la tâche 'style'
+function style() {
+    return gulp.src('scss/**/*.scss') // Chemin vers les fichiers SCSS
+        .pipe(sass().on('error', sass.logError)) // Compilation en CSS
+        .pipe(gulp.dest('./styles')) // Destination des fichiers CSS
+        .pipe(browserSync.stream()); // Injection CSS dans le navigateur
+}
+
+// Déclaration de la tâche 'watch'
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style); // Surveille les changements dans les fichiers SCSS
+    gulp.watch('./*.html').on('change', browserSync.reload); // Recharge le navigateur sur les changements HTML
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload); // Recharge le navigateur sur les changements JS
+}
+
+// Exportation des tâches
+exports.style = style;
+exports.watch = watch;
